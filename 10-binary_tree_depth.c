473a193e175f2b78acb736e494b8237221ee99a8
Olawole Olawale Martins <olawalemart@gmail.com>
2023-08-29 13:53:55 +0100

    tasks 11, 12

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree
- * @tree: pointer to the root node of the tree to measure the height.
+ * @tree: pointer to the root node of the tree to measure the depth.
  * Return: depth of tree
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * size_t binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves.
+ * Return: number of leaves of tree
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	/* recursion guard */
+	if (!tree)
+		return (0);
+	/* if there is no left child or no right child add 1 else 0*/
+	return ((!tree->left && !tree->right ? 1 : 0)
+	+ binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
