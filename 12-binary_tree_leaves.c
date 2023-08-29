c4315895e6f8e30859e7564909c9be14ef7e0e75
Olawole Olawale Martins <olawalemart@gmail.com>
2023-08-29 13:54:53 +0100

    tasks 12 betty correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * size_t binary_tree_leaves - counts the leaves in a binary tree
+ * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves.
  * Return: number of leaves of tree
 */
